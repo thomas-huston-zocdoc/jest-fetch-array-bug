e6ee4b303697b808773b7b0c35546f208d024bf5
Thomas Huston <thomas.huston@zocdoc.com>
2017-01-09 11:14:18 -0500

    Clean up test cases.

--- test.js
+++ test.js
@@ -4,17 +4,25 @@ const fetch = require('node-fetch');
 
 describe('fetch', () => {
 
-    it('cookies with with http', () => {
-        return new Promise((resolve) => {
-            http.get('http://localhost:3000/', (res) => {
-                resolve(res.headers);
-            });
-        }).then((headers) => {
+    const request = () => new Promise((resolve) => {
+        http.get('http://localhost:3000/', (res) => {
+            resolve(res.headers);
+        });
+    })
+
+    it('`set-cookie` header is `instanceof Array`', () => {
+        return request().then((headers) => {
             expect(headers['set-cookie']).to.be.instanceof(Array);
         });
     });
 
-    it('cookies work', () => {
+    it('`set-cookie` header is `Array.isArray`', () => {
+        return request().then((headers) => {
+            expect(Array.isArray(headers['set-cookie'])).to.be.true;
+        });
+    });
+
+    it('`set-cookie` header is returned by `node-fetch`', () => {
         return fetch('http://localhost:3000/')
             .then((response) => {
                 expect(response.headers.get('set-cookie')).to.equal('test=foo; Path=/');
